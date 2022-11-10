ae096539923117ce818bcf3eb7ba0a593536f8ac
Bogdan <ionutbogdan.donici@studenti.unicam.it>
2022-11-10 19:22:20 +0100

    Database added

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
 // Middlewares import
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+// Database Connection
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConn");
 
 const PORT = process.env.PORT || 3500;
+
+console.log(process.env.NODE_ENV.toUpperCase());
+
+connectDB();
 
 // Logger function
 app.use(logger);
@@ -34,6 +42,14 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+mongoose.connection.once("open", () => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
+
+mongoose.connection.on("error", (err) => {
+    console.log(err);
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, "mongoErrLog.log");
 });
